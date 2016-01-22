4c5a6f9fbf0d9288afcafe5c280da3fcc7505a53
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-01-22 10:30:18 +0100

    Added dynamic framework target

---
+++ torrent/DynamicTorrentTV/DynamicTorrentTV.h
@@ -0,0 +1,15 @@
+//
+//  DynamicTorrentTV.h
+//  DynamicTorrentTV
+//
+//  Created by Pedro Pinera Buendia on 22/01/16.
+//  Copyright © 2016 Andrey Chuprina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicTorrentTV.
+FOUNDATION_EXPORT double DynamicTorrentTVVersionNumber;
+
+//! Project version string for DynamicTorrentTV.
+FOUNDATION_EXPORT const unsigned char DynamicTorrentTVVersionString[];
