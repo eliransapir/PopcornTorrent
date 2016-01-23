47e81d99088371b6ddd059fffb8bb8e0cd0bea17
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-01-23 12:06:41 +0100

    Created dynamic library for PopcornTorrent

---
+++ PopcornTorrent/PopcornTorrent.h
@@ -0,0 +1,19 @@
+//
+//  PopcornTorrent.h
+//  PopcornTorrent
+//
+//  Created by Pedro Pinera Buendia on 23/01/16.
+//  Copyright © 2016 io.popcorntimetv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopcornTorrent.
+FOUNDATION_EXPORT double PopcornTorrentVersionNumber;
+
+//! Project version string for PopcornTorrent.
+FOUNDATION_EXPORT const unsigned char PopcornTorrentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopcornTorrent/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 Torrent client for tvOS implemented with `libtorrent`.
 
-## Deploying a new version to CocoaPods
-
-
-
 ## Building static libraries
 
 PopcornTorrent is distributed using CocoaPods and it has internal static libraries as dependencies in `Libtorrent` *(static library)*. These static libraries can be build using the `build.sh` script in the root directory:
